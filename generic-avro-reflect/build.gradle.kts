c8b94cf43293e1c5fee4521789b9e67af7734986
Arvid Heise <arvid.heise@bakdata.com>
2019-02-26 10:51:25 +0100

    Added description to allow (SNAPSHOT) releases

---
+++ generic-avro-reflect/build.gradle.kts
@@ -0,0 +1,10 @@
+description = "Generates an Avro scehma from any Java object at runtime"
+
+dependencies {
+    api(group = "org.apache.avro", name = "avro", version = "1.8.2")
+
+    implementation(group = "org.objenesis", name = "objenesis", version = "3.0.1")
+    implementation(group = "org.mdkt.compiler", name = "InMemoryJavaCompiler", version = "1.3.0")
+
+    implementation(group = "com.google.guava", name = "guava", version = "27.0.1-jre")
+}
